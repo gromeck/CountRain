512a7ac0d8066dc18d4fca8840739eff1af8864a
Christian Lorenz <gromeck@gromeck.de>
2020-06-04 21:43:16 +0200

    changed var names

--- CountRain/http.h
+++ CountRain/http.h
@@ -7,11 +7,12 @@
 #include "util.h"
 #include <ESP8266WebServer.h>
 
-#define HTTP_GET_VAR_WIFI_SSID    "wifi.ssid"
-#define HTTP_GET_VAR_WIFI_PSK     "wifi.psk"
-#define HTTP_GET_VAR_NTP_SERVER   "ntp.server"
-#define HTTP_GET_VAR_COUNTERVAL   "counter.val"
-#define HTTP_GET_VAR_COUNTERINC   "counter.inc"
+#define HTTP_GET_VAR_WIFI_ALLPARAMS "all"
+#define HTTP_GET_VAR_WIFI_SSID      "wifissid"
+#define HTTP_GET_VAR_WIFI_PSK       "wifipsk"
+#define HTTP_GET_VAR_NTP_SERVER     "ntpserver"
+#define HTTP_GET_VAR_COUNTERVAL     "counter"
+#define HTTP_GET_VAR_COUNTERINC     "increment"
 
 /*
 **  setup the HTTP web server:w
